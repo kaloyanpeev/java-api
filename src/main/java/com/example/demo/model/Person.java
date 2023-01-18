10da4b088f7507faaed9fa827c17b18e78a793b9
kaloyanpeev <kaloyan.peev@mentormate.com>
2023-01-18 19:07:54 +0200

    Added notBlank validation for Name property in Person Model

--- src/main/java/com/example/demo/model/Person.java
+++ src/main/java/com/example/demo/model/Person.java
@@ -1,10 +1,15 @@
 package com.example.demo.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+
 
 import java.util.UUID;
+
 public class Person {
   private final UUID id;
+
+  @NotBlank
   private final String name;
 
   public Person(@JsonProperty("id")UUID id,
